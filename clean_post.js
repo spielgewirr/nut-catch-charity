d8cac1950723ec1647789a4fe1e7b7b757a6df39
spielgewirr <spielgewirr@outlook.de>
2026-07-19 22:35:39 +0200

    neuupdate

--- clean_post.js
+++
@@ -1,9 +0,0 @@
-// clean_post.js
-async function run() {
-    await fetch(process.env.DISCORD_WEBHOOK, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ content: "TEST-TEST-TEST" })
-    });
-}
-run();
