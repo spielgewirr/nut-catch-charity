e63a415a4fed170ff85090afd62369518fe3f023
spielgewirr <spielgewirr@outlook.de>
2026-07-20 13:45:32 +0200

    updates

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 const SECRET_PASSWORD = "1aWuselSupport2026";
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
@@ -11,78 +12,139 @@ let dropTimer = 0;
 const bgImg = new Image(); bgImg.src = "bg.png";
 const squirrelImg = new Image(); squirrelImg.src = "squirrel.png";
 const nutImg = new Image(); nutImg.src = "nut.png";
-const goldNutImg = new Image(); goldNutImg.src = "goldnut.png";
 
-const squirrel = { x: 860, y: 860, width: 200, height: 200, speed: 15, movingLeft: false, movingRight: false };
-
-document.addEventListener("keydown", (e) => {
-    if (e.code === "ArrowLeft" || e.code === "KeyA") squirrel.movingLeft = true;
-    if (e.code === "ArrowRight" || e.code === "KeyD") squirrel.movingRight = true;
-});
-document.addEventListener("keyup", (e) => {
-    if (e.code === "ArrowLeft" || e.code === "KeyA") squirrel.movingLeft = false;
-    if (e.code === "ArrowRight" || e.code === "KeyD") squirrel.movingRight = false;
-});
-
-document.getElementById("login-button").onclick = () => {
-    if (document.getElementById("password-input").value === SECRET_PASSWORD) {
-        document.getElementById("login-screen").style.display = "none";
-        document.getElementById("game-container").style.display = "block";
-        startGame();
-    }
+const squirrel = {
+    x: 1920 / 2 - 100,
+    y: 1080 - 220,
+    width: 200,
+    height: 200,
+    speed: 15,
+    movingLeft: false,
+    movingRight: false
 };
 
+document.getElementById("login-button").addEventListener("click", () => {
+    const input = document.getElementById("password-input").value;
+    if (input === SECRET_PASSWORD) {
+        document.getElementById("login-screen").style.display = "none";
+        document.getElementById("game-container").style.display = "flex";
+        startGame();
+    } else {
+        document.getElementById("error-msg").style.display = "block";
+    }
+});
+
 function startGame() {
-    score = 0; timeLeft = 60; nuts = []; gameActive = true;
+    score = 0;
+    timeLeft = 60;
+    nuts = [];
+    gameActive = true;
+    squirrel.x = 1920 / 2 - 100;
+    
     const timerInterval = setInterval(() => {
         if(gameActive) {
             timeLeft--;
             if(timeLeft <= 0) {
-                gameActive = false;
-                clearInterval(timerInterval);
+                endGame(timerInterval);
             }
         }
     }, 1000);
+
     gameLoop();
+}
+
+window.addEventListener("keydown", (e) => {
+    if (e.code === "ArrowLeft" || e.code === "KeyA") squirrel.movingLeft = true;
+    if (e.code === "ArrowRight" || e.code === "KeyD") squirrel.movingRight = true;
+});
+window.addEventListener("keyup", (e) => {
+    if (e.code === "ArrowLeft" || e.code === "KeyA") squirrel.movingLeft = false;
+    if (e.code === "ArrowRight" || e.code === "KeyD") squirrel.movingRight = false;
+});
+window.addEventListener("touchstart", (e) => {
+    if(!gameActive) return;
+    const touchX = e.touches[0].clientX;
+    const screenWidth = window.innerWidth;
+    if(touchX < screenWidth / 2) squirrel.movingLeft = true;
+    else squirrel.movingRight = true;
+});
+window.addEventListener("touchend", () => {
+    squirrel.movingLeft = false;
+    squirrel.movingRight = false;
+});
+
+function spawnNut() {
+    nuts.push({
+        x: Math.random() * (1920 - 60),
+        y: -60,
+        width: 60,
+        height: 60,
+        speed: Math.random() * 5 + 8
+    });
 }
 
 function updateGame() {
     if (squirrel.movingLeft && squirrel.x > 0) squirrel.x -= squirrel.speed;
-    if (squirrel.movingRight && squirrel.x < 1720) squirrel.x += squirrel.speed;
-    
+    if (squirrel.movingRight && squirrel.x < 1920 - squirrel.width) squirrel.x += squirrel.speed;
+
     dropTimer++;
     if (dropTimer > 30) {
-        nuts.push({ x: Math.random() * 1860, y: -60, type: Math.random() < 0.1 ? 'gold' : 'normal' });
+        spawnNut();
         dropTimer = 0;
     }
-    
+
     for (let i = nuts.length - 1; i >= 0; i--) {
-        nuts[i].y += 8;
-        if (nuts[i].y > 860 && nuts[i].x > squirrel.x - 50 && nuts[i].x < squirrel.x + 150) {
-            score += (nuts[i].type === 'gold' ? 50 : 10);
+        let n = nuts[i];
+        n.y += n.speed;
+
+        if (n.x < squirrel.x + squirrel.width &&
+            n.x + n.width > squirrel.x &&
+            n.y < squirrel.y + squirrel.height &&
+            n.y + n.height > squirrel.y) {
+            
+            score += 10;
             nuts.splice(i, 1);
-        } else if (nuts[i].y > 1080) nuts.splice(i, 1);
+            continue;
+        }
+
+        if (n.y > 1080) {
+            nuts.splice(i, 1);
+        }
     }
 }
 
 function drawGame() {
     ctx.clearRect(0, 0, 1920, 1080);
     if(bgImg.complete) ctx.drawImage(bgImg, 0, 0, 1920, 1080);
-    ctx.drawImage(squirrelImg, squirrel.x, squirrel.y, 200, 200);
-    for (let n of nuts) ctx.drawImage(n.type === 'gold' ? goldNutImg : nutImg, n.x, n.y, 60, 60);
-    
+
+    if(squirrelImg.complete) {
+        ctx.drawImage(squirrelImg, squirrel.x, squirrel.y, squirrel.width, squirrel.height);
+    } else {
+        ctx.fillStyle = "orange";
+        ctx.fillRect(squirrel.x, squirrel.y, squirrel.width, squirrel.height);
+    }
+
+    for (let n of nuts) {
+        if(nutImg.complete) {
+            ctx.drawImage(nutImg, n.x, n.y, n.width, n.height);
+        } else {
+            ctx.fillStyle = "saddlebrown";
+            ctx.fillRect(n.x, n.y, n.width, n.height);
+        }
+    }
+
     ctx.font = "bold 60px sans-serif";
-    ctx.lineWidth = 3;
+    ctx.lineWidth = 5;
     ctx.strokeStyle = "black";
     ctx.fillStyle = "white";
-    
+
     ctx.textAlign = "left";
     ctx.strokeText("Punkte: " + score, 40, 80);
     ctx.fillText("Punkte: " + score, 40, 80);
-    
+
     ctx.textAlign = "right";
-    ctx.strokeText("Zeit: " + timeLeft + "s", 1880, 80);
-    ctx.fillText("Zeit: " + timeLeft + "s", 1880, 80);
+    ctx.strokeText("Zeit: " + timeLeft + "s", 1920 - 40, 80);
+    ctx.fillText("Zeit: " + timeLeft + "s", 1920 - 40, 80);
 }
 
 function gameLoop() {
@@ -90,4 +152,47 @@ function gameLoop() {
     updateGame();
     drawGame();
     requestAnimationFrame(gameLoop);
+}
+
+function endGame(timerInterval) {
+    gameActive = false;
+    clearInterval(timerInterval);
+    displayHighscores();
+}
+
+function displayHighscores() {
+    const highscoreScreen = document.getElementById("highscore-screen");
+    const list = document.getElementById("highscore-list");
+    const countdownText = document.getElementById("countdown-text");
+    const restartButton = document.getElementById("restart-button");
+    const heading = document.querySelector("#highscore-screen h2");
+
+    highscoreScreen.style.display = "block";
+    if (heading) heading.innerText = "Dein Score";
+    
+    list.innerHTML = `<li style="list-style: none; margin-top: 20px; font-size: 24px;">
+        Du hast gerade <b>${score} Punkte</b> gesammelt! 🐿️
+    </li>`;
+
+    restartButton.style.display = "none";
+    countdownText.style.display = "block";
+
+    let waitTime = 10;
+    countdownText.innerText = `Nächste Runde möglich in ${waitTime} Sekunden...`;
+    
+    const countdownInterval = setInterval(() => {
+        waitTime--;
+        if(waitTime > 0) {
+            countdownText.innerText = `Nächste Runde möglich in ${waitTime} Sekunden...`;
+        } else {
+            clearInterval(countdownInterval);
+            countdownText.style.display = "none";
+            restartButton.style.display = "inline-block";
+        }
+    }, 1000);
+
+    restartButton.onclick = () => {
+        highscoreScreen.style.display = "none";
+        startGame();
+    };
 }
